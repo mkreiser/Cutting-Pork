d668eea926e6397570a1a4a4354ccca4c3d45ca9
mkreiser <mkreise2@illinois.edu>
2016-04-06 18:55:26 -0500

    Push angular deployment

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cuttingPorkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cuttingPorkApp
+ */
+angular.module('cuttingPorkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cuttingPorkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cuttingPorkApp
+ */
+angular.module('cuttingPorkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
